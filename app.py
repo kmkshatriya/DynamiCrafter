737617802ac51396df563049be4230eb9a42c05e
kmkshatriya <139339313+kmkshatriya@users.noreply.github.com>
2024-09-05 20:00:16 +0530

    up

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ class Image2Video():
         
         self.resolution = (int(resolution.split('_')[0]), int(resolution.split('_')[1])) #hw
 
-        self.download_model()
         ckpt_path=ckpt_dir+'/dynamicrafter_'+resolution.split('_')[1]+self.suffix+'_v1/model.ckpt'
         # ckpt_path=f'{ckpt_dir}/dynamicrafter_512_interp_fp16_pruned.safetensors'
         # ckpt_path=f'{ckpt_dir}/dynamicrafter_512_fp16_pruned.safetensors'        
@@ -35,16 +34,21 @@ class Image2Video():
         config = OmegaConf.load(config_file)
         model_config = config.pop("model", OmegaConf.create())
         model_config['params']['unet_config']['params']['use_checkpoint']=False   
-        model_list = []
-        for gpu_id in range(gpu_num):
-            model = instantiate_from_config(model_config)
-            model = model.cuda(gpu_id)
-            model.perframe_ae = True if not self.resolution[1] ==256 else False
-            assert os.path.exists(ckpt_path), "Error: checkpoint Not Found!"
-            model = load_model_checkpoint(model, ckpt_path)
-            model.eval()
-            model_list.append(model)
-        self.model_list = model_list
+
+        model = instantiate_from_config(model_config)
+
+        if torch.cuda.device_count() > 1 and gpu_num > 1:
+            print(f"Using {torch.cuda.device_count()} GPUs!")
+            model = torch.nn.DataParallel(model)
+
+
+        model = model.cuda()
+        model.perframe_ae = True if not self.resolution[1] ==256 else False
+        assert os.path.exists(ckpt_path), "Error: checkpoint Not Found!"
+        model = load_model_checkpoint(model, ckpt_path)
+        model.eval()
+        model_list.append(model)
+
         self.save_fps = 8
 
     def get_image(self, image_path, prompt, result, steps=50, cfg_scale=7.5, eta=1.0, seed=123):
@@ -60,24 +64,28 @@ class Image2Video():
         transform = transforms.Compose([
             transforms.Resize(min(self.resolution)),
             transforms.CenterCrop(self.resolution),
-            ])
+        ])
         torch.cuda.empty_cache()
         print('start:', prompt, time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
         start = time.time()
-        gpu_id=0
+
         if steps > 60:
             steps = 60 
-        model = self.model_list[gpu_id]
+
+        model = self.model
+
+        # Ensure the model is on the proper device
         model = model.cuda()
-        batch_size=1
-        channels = model.model.diffusion_model.out_channels
-        frames = model.temporal_length
+        
+        batch_size = 1
+        channels = model.module.model.diffusion_model.out_channels if isinstance(model, torch.nn.DataParallel) else model.model.diffusion_model.out_channels
+        frames = model.module.temporal_length if isinstance(model, torch.nn.DataParallel) else model.temporal_length
         h, w = self.resolution[0] // 8, self.resolution[1] // 8
         noise_shape = [batch_size, channels, frames, h, w]
 
         # text cond
         with torch.no_grad(), torch.cuda.amp.autocast():
-            text_emb = model.get_learned_conditioning([prompt])
+            text_emb = model.module.get_learned_conditioning([prompt]) if isinstance(model, torch.nn.DataParallel) else model.get_learned_conditioning([prompt])
 
             # img cond
             image = Image.open(image_path).convert('RGB')
@@ -85,11 +93,11 @@ class Image2Video():
             img_tensor = torch.from_numpy(image).permute(2, 0, 1).float().to(model.device)
             img_tensor = (img_tensor / 255. - 0.5) * 2
 
-            image_tensor_resized = transform(img_tensor) #3,h,w
-            videos = image_tensor_resized.unsqueeze(0) # bchw
-            
-            z = get_latent_z(model, videos.unsqueeze(2)) #bc,1,hw
-            
+            image_tensor_resized = transform(img_tensor) # 3,h,w
+            videos = image_tensor_resized.unsqueeze(0)  # bchw
+
+            z = get_latent_z(model.module, videos.unsqueeze(2)) if isinstance(model, torch.nn.DataParallel) else get_latent_z(model, videos.unsqueeze(2)) # bc,1,hw
+
             img_tensor_repeat = repeat(z, 'b c t h w -> b c (repeat t) h w', repeat=frames)
 
             if self.is_interp:
@@ -97,8 +105,8 @@ class Image2Video():
                 img_tensor_repeat[:,:,:1,:,:] = z
                 img_tensor_repeat[:,:,-1:,:,:] = z
 
-            cond_images = model.embedder(img_tensor.unsqueeze(0)) ## blc
-            img_emb = model.image_proj_model(cond_images)
+            cond_images = model.module.embedder(img_tensor.unsqueeze(0)) if isinstance(model, torch.nn.DataParallel) else model.embedder(img_tensor.unsqueeze(0)) ## blc
+            img_emb = model.module.image_proj_model(cond_images) if isinstance(model, torch.nn.DataParallel) else model.image_proj_model(cond_images)
 
             imtext_cond = torch.cat([text_emb, img_emb], dim=1)
 
@@ -119,16 +127,6 @@ class Image2Video():
         model = model.cpu()
         return os.path.join(result_dir, out_vid_nm)
     
-    def download_model(self):
-        REPO_ID = 'Doubiiu/DynamiCrafter_'+str(self.resolution[1])+self.suffix if self.resolution[1]!=256 else 'Doubiiu/DynamiCrafter'
-        filename_list = ['model.ckpt']
-        if not os.path.exists('./'+self.ckpt_dir+'/dynamicrafter_'+str(self.resolution[1])+self.suffix+'_v1/'):
-            os.makedirs('./'+self.ckpt_dir+'/dynamicrafter_'+str(self.resolution[1])+self.suffix+'_v1/')
-        for filename in filename_list:
-            local_file = os.path.join('./'+self.ckpt_dir+'/dynamicrafter_'+str(self.resolution[1])+self.suffix+'_v1/', filename)
-            if not os.path.exists(local_file):
-                hf_hub_download(repo_id=REPO_ID, filename=filename, local_dir='./'+self.ckpt_dir+'/dynamicrafter_'+str(self.resolution[1])+self.suffix+'_v1/', local_dir_use_symlinks=False)
-
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", type=str, default="prompts/512/girl08.png", help="Path to input image")
@@ -138,7 +136,6 @@ def get_parser():
     parser.add_argument("--height", type=int, default=320, help="image height, in pixel space")
     parser.add_argument("--seed", type=int, default=42, help="Seed value for video generation")
     parser.add_argument('--interp', action='store_true', help="Enable interpolation or not")
-    parser.add_argument('--gpus', type=int, default=1, help="No of gpus to use")
     parser.add_argument("--model", type=str, default="checkpoints", help="Path to checkpoints folder")
 
     return parser
@@ -146,6 +143,7 @@ def get_parser():
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    i2v = Image2Video(f"{args.height}_{args.width}", args.interp, args.gpus, args.model)
+    gpus = torch.cuda.device_count()
+    i2v = Image2Video(f"{args.height}_{args.width}", args.interp, gpus, args.model)
     video_path = i2v.get_image(args.image, args.prompt, args.result, seed=args.seed)
     print('done', video_path)
